d0251278d7c856f8aae16f331a289446dc822497
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-01-30 12:56:50 -0300

    first commit

---
+++ src/com/bibs/estruturadados/teste/Aula02.java
@@ -0,0 +1,17 @@
+package com.bibs.estruturadados.teste;
+
+import com.bibs.estruturadados.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+
+		Vetor vetorTeste = new Vetor(5);
+		
+		int i = 0;
+		i++;
+
+	}
+
+}
---
+++ src/com/bibs/estruturadados/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.bibs.estruturadados.teste;
+
+import com.bibs.estruturadados.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+	}
+
+}
