eaf3ec0d830d14e1aa3500d2661745a3b85b9158
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-01-30 14:48:14 -0300

    exercícios aulas 3 e 4

--- src/com/bibs/estruturadados/teste/Aula03.java
+++ src/com/bibs/estruturadados/teste/Aula03.java
@@ -8,8 +8,17 @@ public class Aula03 {
 		
 		Vetor vetor = new Vetor(5);
 
+//		try {
+//			vetor.adiciona("elemento 1");
+//			vetor.adiciona("elemento 2");
+//			vetor.adiciona("elemento 3");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+				
 		vetor.adiciona("elemento 1");
 		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
 	}
 
 }
