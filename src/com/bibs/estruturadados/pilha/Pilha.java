6368a528d37a83b9d8c83f06c286d16ca8b00772
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-02-07 17:45:43 -0300

    pilhas + aula13

---
+++ src/com/bibs/estruturadados/pilha/Pilha.java
@@ -0,0 +1,15 @@
+package com.bibs.estruturadados.pilha;
+
+import com.bibs.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+	
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+
+}
