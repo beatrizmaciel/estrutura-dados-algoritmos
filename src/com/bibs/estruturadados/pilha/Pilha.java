a51d501d6a96ac33390e45d9785d4564448048bd
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-02-07 20:01:56 -0300

    add lessons 16 and 17

--- src/com/bibs/estruturadados/pilha/Pilha.java
+++ src/com/bibs/estruturadados/pilha/Pilha.java
@@ -15,5 +15,24 @@ public class Pilha<T> extends EstruturaEstatica<T> {
 	public void empilha(T elemento) {
 		super.adiciona(elemento);
 	}
+	
+	public T topo() {
+		if (this.estaVazio()) {
+			return null;
+		}
+		
+		return this.elementos[tamanho-1];
+	}
+	
+	public T desempilha() {
+		
+		if(this.estaVazio()) {
+			return null;
+		}
+		T elemento = this.elementos[tamanho-1];
+		tamanho--;
+		
+		return elemento;
+	}
 
 }
---
+++ src/com/bibs/estruturadados/pilha/teste/Aula16.java
@@ -0,0 +1,25 @@
+package com.bibs.estruturadados.pilha.teste;
+
+import com.bibs.estruturadados.pilha.Pilha;
+
+public class Aula16 {
+
+	public static void main(String[] args) {
+		
+		Pilha<Integer> pilha = new Pilha<Integer>();
+	
+		System.out.println(pilha.topo());
+		
+		System.out.println(pilha);
+		
+		pilha.empilha(1);
+		
+		pilha.empilha(2);
+		
+		System.out.println(pilha.topo());
+		
+		System.out.println(pilha);
+		
+	}
+
+}
